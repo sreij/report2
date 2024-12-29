b7f7e41989f8e00da973a8d56e8f55823951826b
sreij <chrj23020@g.nihon-u.ac.jp>
2024-12-29 22:41:10 +0900

    update footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,14 +1,14 @@
 export default function Footer() {
     return (
         <footer>
-            <p>文理学部情報科学科<br />Webプログラミング最終課題</p>
+            <p>日本大学文理学部情報科学科<br />Webプログラミング最終課題</p>
             <p>5423020<br />齋藤礼二</p>
             <div>
                 <h3>使用API</h3>
-                <li>Ant-Design</li>
-                <li>GeoLocation</li>
-                <li>GeoNames</li>
-                <li>WeatherAPI</li>
+                <li><a hrel="https://ant.design/">Ant-Design</a></li>
+                <li><a hrel="https://developer.mozilla.org/ja/docs/Web/API/Geolocation">GeoLocation</a></li>
+                <li><a hrel="https://www.geonames.org/">GeoNames</a></li>
+                <li><a hrel="https://www.weatherapi.com/">WeatherAPI</a></li>
             </div>
         </footer>
     );
